f8bfa628ad249127266d6a98d762f4d844dd7377
shinadhmuhammed <muhammedshinad2@gmail.com>
2025-08-25 22:51:12 +0530

    fixed the bugs and deploying

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({
    origin: [
     "http://localhost:8080",
-    "https://shavesync-pro.vercel.app"
+    "https://cutclub.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, 
